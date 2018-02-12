1a80a780f7022f3b8fbadb10128d7b0b94a2fc05
manuelhernaws <36063620+manuelhernaws@users.noreply.github.com>
2018-02-11 18:59:54 -0800

    Assignment 01
    COMM 644 JavaScript Essentials

---
+++ js/script.js
@@ -0,0 +1,215 @@
+/*eslint-env browser*/
+
+// STEP 1
+/*
+var someMonth;
+function theMonth 		// function to return current month
+currentMonth 		// a constant
+var summerMonth; 		// an array of summer months
+myLibrary_afunction 	// a function
+*/
+
+// STEP 2
+/*
+2.1      // numerc literal
+"hi"     // string literal
+true     // boolean literal
+null     // literal null value
+*/
+
+// step 3
+/*
+var anexpression = 4 * 8 + 2; // variable Expression
+
+var onePlusFiveI = Complex(1, 0)['+'](Complex(0, 5));
+*/
+
+// step 4
+/*var firstName;
+var lastName;
+var Address;
+var City;
+var State;
+var Zip_Code;
+var Your_Age;
+var Referral_Source;
+var May_We_Contact_You;
+*/
+
+// step 5
+/*
+var firstName = "manny"
+var lastName =  "Hernandez"
+var Address = "clairemont"
+var City = "san diego"
+var State = "california"
+var Zip_Code = "92117"
+var Your_Age = "28"
+var Referral_Source = "google"
+var May_We_Contact_You = "true"
+
+var lastName;
+var Address;
+var City;
+var State;
+var Zip_Code;
+var Your_Age;
+var Referral_Source;
+var May_We_Contact_You;
+firstName = "manny"
+lastName =  "Hernandez"
+Address = "clairemont"
+City = "san diego"
+State = "california"
+Zip_Code = "92117"
+Your_Age = "28"
+Referral_Source = "google"
+May_We_Contact_You = "true"
+
+var firstName = "manny", lastName =  "Hernandez", Address = "clairemont", City = "san diego", State = "california", Zip_Code = "92117", Your_Age = "28", Referral_Source = "google", May_We_Contact_You = "true";
+
+*/
+
+// step 6
+/*
+var yearsMarried = "ten";
+window.console.log("manny has been married for " + yearsMarried + " years");
+
+var yearsMarried = (5 + 5);
+window.console.log("manny has been married for " + yearsMarried + " years");
+
+var yearsMarried = 2 * (5);
+window.console.log("manny has been married for " + yearsMarried + " years");
+*/
+
+// step 7
+/*
+var car;
+window.console.log(car);
+car = "chevy";
+*/
+
+// step 8
+/*
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);
+*/
+
+// step 9
+/*
+var x = 100;
+var y = null;
+window.console.log(y);
+*/
+
+/*var y;
+window.console.log(y)
+*/
+
+// step 10
+/*
+window.console.log(typeof "manny");
+window.console.log(typeof 666);
+window.console.log(typeof true);
+window.console.log(typeof mydog);
+*/
+
+// step 11
+/*
+var myname= "Manuel Hernandez";
+window.alert("Hello " + myname + ", welcome to the JavaScript class!")
+*/
+
+// step 12
+/*
+var name = "Manuel";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+// step 13
+/*var name = "Manuel";
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+//step 14
+/*
+var name = "Manuel";
+var course = "JavaScript";
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+//step 15
+/*
+var name = prompt("Please enter your name");
+var course = "JavaScript";
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+// Step 16
+/*
+var name = prompt("Please enter your name");
+var course = prompt("What Class Are You Taking")
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+// Step 17
+/*
+x = 10;
+y = 20;
+window.console.log(x + y);
+*/
+
+// Step 18
+/*
+var x = 20 + 20;
+window.console.log(x);
+*/
+
+// Step 19
+/*
+var x = 20;
+var y = 5;
+window.console.log(x * y);
+*/
+
+// Step 20
+/*
+var x = 6.6/6.6 + 1;
+window.console.log(x);
+*/
+
+// Step 21
+/*
+var x = 5;
+var y = 5;
+window.console.log(x == 5 || y == 5);
+*/
+
+// Step 22
+/*
+var x = 20;
+var y = 10;
+window.console.log(x<=y != false);
+*/
+
+// Step 23
+/*
+var widget = new Object();
+window.console.log(typeof widget);
+*/
+
+// Step 24
+/*
+var widget = new Object();
+window.console.log(widget instanceof Object);
+*/
+
+// Step 25
+/*
+function object() {}
+function object1() {}
+
+var widget = new Object();
+window.console.log(widget instanceof object1);
+*/
